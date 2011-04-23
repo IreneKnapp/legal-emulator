d266cb0b3d7022f37c76fb71bff4606de271fdee
Irene Knapp <irene.knapp@icloud.com>
2011-04-23 08:49:15 +0000

    Front-end stuff, mostly copyright notices and graphics changes.

--- FrontEnd/AppDelegate.h
+++ FrontEnd/AppDelegate.h
@@ -3,15 +3,16 @@
 //  Legal Emulator
 //
 //  Created by Dan Knapp on 4/21/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Dan Knapp. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
-@interface Legal_EmulatorAppDelegate : NSObject <NSApplicationDelegate> {
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
 }
-
 @property (assign) IBOutlet NSWindow *window;
 
+- (IBAction) addGames: (id) sender;
+- (IBAction) removeGames: (id) sender;
 @end
--- FrontEnd/main.m
+++ FrontEnd/main.m
@@ -3,7 +3,7 @@
 //  Legal Emulator
 //
 //  Created by Dan Knapp on 4/21/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Dan Knapp. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
