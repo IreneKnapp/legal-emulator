3cb4c2635147bf66342e4e1370334386b4aa6af4
Irene Knapp <irene.knapp@icloud.com>
2011-05-07 05:34:47 +0000

    For some reason this stopped working and then started working again :(

--- BackEnd/EmulatorExtras.c
+++ BackEnd/EmulatorExtras.c
@@ -1,15 +1,51 @@
 #include "HsFFI.h"
 #include "Emulator_stub.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
 
 extern void __stginit_legalzmemulatorzm1zi0_Emulator();
 
 
 int emulator_init() {
-  int argc = 1;
-  char *arg0 = "emulator";
-  char **argv = { &arg0 };
+  int profiling = 0;
   
-  hs_init(&argc, &argv);
+  char *profiling_variable = getenv("LEGAL_EMULATOR_PROFILE");
+  if(profiling_variable && !strcasecmp(profiling_variable, "yes")) {
+    printf("Profiling, hopefully.\n");
+    profiling = 1;
+  }
+  
+  if(profiling) {
+    int argc = 3;
+    char *arg0 = "emulator";
+    char *arg1 = "+RTS";
+    char *arg2 = "-hy";
+    char **argv = malloc(4 * sizeof(char *));
+    argv[0] = arg0;
+    argv[1] = arg1;
+    argv[2] = arg2;
+    argv[3] = NULL;
+    int argc_modified = argc;
+    char **argv_modified = argv;
+    
+    hs_init(&argc_modified, &argv_modified);
+    
+    free(argv);
+  } else {
+    int argc = 1;
+    char *arg0 = "emulator";
+    char **argv = malloc(2 * sizeof(char *));
+    argv[0] = arg0;
+    argv[1] = NULL;
+    int argc_modified = argc;
+    char **argv_modified = argv;
+    
+    hs_init(&argc_modified, &argv_modified);
+    
+    free(argv);
+  }
+  
   hs_add_root(__stginit_legalzmemulatorzm1zi0_Emulator);
   
   return 1;
